d6f809da4d643f06618d57bbdc21014248ef4602
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-08-22 16:53:42 -0700

    Update feedstock/recipe.py

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern
 from pangeo_forge_recipes.recipes import XarrayZarrRecipe
 
-# add comment to trigger ci - q
+# add comment to trigger ci - qw
 start_date = "1981-09-01"
 
 
